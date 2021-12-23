e23b5e3952902f1ac58c0ff74b4b51e45b8de819
hayatulemon <hayatulemon340@gmail.com>
2021-12-23 19:04:40 +0600

    maat server

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+
+const app = express();
+const port = process.env.PORT || 4040;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.r9gms.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+console.log(uri);
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db("Maat");
+        const teamsCollection = database.collection("teams");
+        const expertiseCollection = database.collection("expertise");
+
+        // all teams 
+        app.get('/teams', async (req, res) => {
+            const teams = await teamsCollection.find({}).toArray();
+            res.send(teams);
+
+        });
+
+        // all teams 
+        app.get('/expertise', async (req, res) => {
+            const expertise = await expertiseCollection.find({}).toArray();
+            res.send(expertise);
+        });
+    }
+
+    finally {
+
+    }
+} run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
